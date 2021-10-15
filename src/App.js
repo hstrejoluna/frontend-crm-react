655fd46bcfa77c00e22eb250357d206d1a2348f3
hstrejoluna <trejolunatutoriales@gmail.com>
2021-10-15 15:37:23 -0500

    creating first components

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+import Header from "./componentes/layout/Header";
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+
+      <div class="grid contenedor contenido-principal"></div>
+    </Fragment>
+  );
+}
+export default App;
