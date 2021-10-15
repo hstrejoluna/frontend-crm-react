531b1ef5749608da4d13fa47acaa1e4c729c2715
hstrejoluna <trejolunatutoriales@gmail.com>
2021-10-15 15:44:52 -0500

    creating navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Fragment } from "react";
 import Header from "./componentes/layout/Header";
+import Navegacion from "./componentes/layout/Navegacion";
 
 function App() {
   return (
     <Fragment>
       <Header />
 
-      <div class="grid contenedor contenido-principal"></div>
+      <div class="grid contenedor contenido-principal">
+        <Navegacion />
+      </div>
     </Fragment>
   );
 }
